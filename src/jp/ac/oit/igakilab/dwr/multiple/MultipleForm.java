0a30b0a8ba48d6a89c15c664329ec4a6f1813b33
Hiroshi Igaki <github@musictale.net>
2015-07-08 20:12:01 +0900

    DWRで利用する場合の注意事項をコメントとして追加

--- src/jp/ac/oit/igakilab/dwr/multiple/MultipleForm.java
+++ src/jp/ac/oit/igakilab/dwr/multiple/MultipleForm.java
@@ -1,6 +1,14 @@
 package jp.ac.oit.igakilab.dwr.multiple;
 
 
+/**
+ * JavaScriptから渡されるオブジェクト．DWRを利用してJavaScriptからフォームをオブジェクトとして渡す場合，下記を満たしている必要がある．
+ * 1. すべてのフィールドがprivateであり，publicなsetter/getterを持っている
+ * 2. publicなデフォルトコンストラクタ（引数を持たないコンストラクタ）が存在する
+ * 3. クラスがpublicである
+ * @author Hiroshi
+ *
+ */
 public class MultipleForm{
     private int max = 20;
     private int multiple = 3;
