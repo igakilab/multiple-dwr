3a0981dc59803718febedc247c70242c94fd45f6
Hiroshi Igaki <github@musictale.net>
2019-08-24 23:01:06 +0900

    jsからjavaメソッド呼び出し

--- src/main/java/jp/igakilab/dwr/multiple/MultiplePrinter.java
+++ src/main/java/jp/igakilab/dwr/multiple/MultiplePrinter.java
@@ -1,5 +1,7 @@
 package jp.igakilab.dwr.multiple;
 
+import java.util.ArrayList;
+
 /**
  * DWRでJSから呼ばれるメソッドはすべてpublicでなければならない．また，必要なクラスはすべてdwr.xmlに定義されている必要がある．
  *
@@ -7,6 +9,30 @@ package jp.igakilab.dwr.multiple;
  *
  */
 public class MultiplePrinter {
+  /**
+   * 1からmaxまでのすべての数値について， 以下の条件を満たしながら小さい順にリスト化して返す
+   * 条件1：数値がmultipleの倍数のときは”ryokun”をリストに追加する．
+   *
+   * @return 1から10までの数値を変換した文字列のリスト
+   */
+  public ArrayList<String> execute(MultipleForm input) throws InvalidValueException {
+    ArrayList<String> list = new ArrayList<>();
+    int max = input.getMax();
+    int multiple = input.getMultiple();
+
+    if (multiple <= 0 || max < 0) {
+      throw new InvalidValueException("倍数及び最大値は 正の整数(>0)でなければいけません．");
+    }
+    for (int i = 1; i <= max; i++) {
+      if (i % multiple == 0) {
+        list.add("ryokun");
+      } else {
+        list.add(Integer.toString(i));
+      }
+    }
+    return list;
+  }
+
   /**
    * REST呼び出しを行う場合は下記のように呼ぶ
    * http://sample.com:8080/project_name/dwr/jsonp/ClassName/MethodName/param1/
